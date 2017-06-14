98bfc51192242d9da56efe5b56c909e813897acd
wxywr <1261046195@qq.com>
2017-06-14 15:10:06 +0800

    update

--- js/imgUp.js
+++ js/imgUp.js
@@ -60,8 +60,123 @@ $(function(){
 		//input内容清空
 		$(this).val("");
 	});
+
+	///////////////////////////
+
+	$(".files").change(function(){
+		var idFile = $(this).attr("id");
+		var file = document.getElementById(idFile);
+		var imgContainer = $(this).parents(".z_photo"); //存放图片的父亲元素
+		var fileList = file.files; //获取的图片文件
+		console.log(fileList+"======filelist=====");
+		var input = $(this).parent();//文本框的父亲元素
+		var imgArr = [];
+		//遍历得到的图片文件
+		var numUp = imgContainer.find(".up-section").length;
+		var totalNum = numUp + fileList.length;  //总的数量
+		if(fileList.length > 3 || totalNum > 3 ){
+			alert("上传图片数目不可以超过3个，请重新选择");  //一次选择上传超过3个 或者是已经上传和这次上传的到的总数也不可以超过3个
+		}
+		else if(numUp < 3){
+			fileList = validateUp(fileList);
+			for(var i = 0;i<fileList.length;i++){
+			 var imgUrl = window.URL.createObjectURL(fileList[i]);
+			     imgArr.push(imgUrl);
+			 var $section = $("<section class='up-section fl loading'>");
+			     imgContainer.prepend($section);
+			 var $span = $("<span class='up-span'>");
+			     $span.appendTo($section);
+			
+		     var $img0 = $("<img class='close-upimg'>").on("click",function(event){
+				    event.preventDefault();
+					event.stopPropagation();
+					$(".works-mask").show();
+					delParent = $(this).parent();
+				});   
+				$img0.attr("src","img/a7.png").appendTo($section);
+		     var $img = $("<img class='up-img up-opcity'>");
+		         $img.attr("src",imgArr[i]);
+		         $img.appendTo($section);
+		     var $p = $("<p class='img-name-p'>");
+		         $p.html(fileList[i].name).appendTo($section);
+		     var $input = $("<input id='taglocation' name='taglocation' value='' type='hidden'>");
+		         $input.appendTo($section);
+		     var $input2 = $("<input id='tags' name='tags' value='' type='hidden'/>");
+		         $input2.appendTo($section);
+		      
+		   }
+		}
+		setTimeout(function(){
+             $(".up-section").removeClass("loading");
+		 	 $(".up-img").removeClass("up-opcity");
+		 },450);
+		 numUp = imgContainer.find(".up-section").length;
+		if(numUp >= 3){
+			// $(this).parent().hide();
+			$('.add-imgs').attr('src','images/chuan.png');
+		}
+		//input内容清空
+		$(this).val("");
+	});
+
+	///////////////////////
+	$(".filess").change(function(){
+		var idFile = $(this).attr("id");
+		var file = document.getElementById(idFile);
+		var imgContainer = $(this).parents(".z_photo"); //存放图片的父亲元素
+		var fileList = file.files; //获取的图片文件
+		console.log(fileList+"======filelist=====");
+		var input = $(this).parent();//文本框的父亲元素
+		var imgArr = [];
+		//遍历得到的图片文件
+		var numUp = imgContainer.find(".up-section").length;
+		var totalNum = numUp + fileList.length;  //总的数量
+		if(fileList.length > 3 || totalNum > 3 ){
+			alert("上传图片数目不可以超过3个，请重新选择");  //一次选择上传超过3个 或者是已经上传和这次上传的到的总数也不可以超过3个
+		}
+		else if(numUp < 3){
+			fileList = validateUp(fileList);
+			for(var i = 0;i<fileList.length;i++){
+			 var imgUrl = window.URL.createObjectURL(fileList[i]);
+			     imgArr.push(imgUrl);
+			 var $section = $("<section class='up-section fl loading'>");
+			     imgContainer.prepend($section);
+			 var $span = $("<span class='up-span'>");
+			     $span.appendTo($section);
+			
+		     var $img0 = $("<img class='close-upimg'>").on("click",function(event){
+				    event.preventDefault();
+					event.stopPropagation();
+					$(".works-mask").show();
+					delParent = $(this).parent();
+				});   
+				$img0.attr("src","img/a7.png").appendTo($section);
+		     var $img = $("<img class='up-img up-opcity'>");
+		         $img.attr("src",imgArr[i]);
+		         $img.appendTo($section);
+		     var $p = $("<p class='img-name-p'>");
+		         $p.html(fileList[i].name).appendTo($section);
+		     var $input = $("<input id='taglocation' name='taglocation' value='' type='hidden'>");
+		         $input.appendTo($section);
+		     var $input2 = $("<input id='tags' name='tags' value='' type='hidden'/>");
+		         $input2.appendTo($section);
+		      
+		   }
+		}
+		setTimeout(function(){
+             $(".up-section").removeClass("loading");
+		 	 $(".up-img").removeClass("up-opcity");
+		 },450);
+		 numUp = imgContainer.find(".up-section").length;
+		if(numUp >= 3){
+			// $(this).parent().hide();
+			$('.add-imgss').attr('src','images/chuan.png');
+		}
+		//input内容清空
+		$(this).val("");
+	});
 	
-	
+	////////////////////
    
     $(".z_photo").delegate(".close-upimg","click",function(){
      	  $(".works-mask").show();
